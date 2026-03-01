9f9d87beebb37ceadf15cccb98cd74a7e0938f5e
rv-work <rv.dthought@gmail.com>
2026-03-01 17:31:42 +0530

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import cors from 'cors';
 import connectDB from './DB/dbConnect.js';
-import TrainRoutes from './Routes/TrainRoutes.js'
-
+import TrainRoutes from './Routes/TrainRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -10,19 +10,23 @@ const app = express();
 
 const PORT = process.env.PORT || 5100;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://train-ticket-rvn.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
-
 app.use("/api/trains", TrainRoutes);
 
-
-app.listen(PORT , () => {
-  console.log(`Server is running at : http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
